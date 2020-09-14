8f0db47d9c8bc5fed0940fd56b906b1ec17ad029
vldln-biven'мамы.net <strifee322@gmail.com>
2020-09-14 08:58:28 +0500

    dict

---
+++ 200912_homework2.py
@@ -0,0 +1,15 @@
+my_list = ['v', 'l', 'a', 'd', 'l', 'e', 'n']
+my_dict = {'v': 1, 'l': 2, 'a': 3, 'd': 4, 'e': 5, 'n': 6}
+
+print (my_list)
+print (my_dict)
+
+print (len(my_list))
+print (len(my_dict))
+print (len('vl a'))
+
+my_list = ['v', 'l', 'a', 'd', 'l', 'e' 'n']
+print('a' in my_list)           # True
+print('q' in my_list)           # False
+print('a' not in my_list)       # False
+print('q' not in my_list)       # True
